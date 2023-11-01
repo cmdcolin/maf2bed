97ca5e0303015481773bfc4f260d1b722f988818
Colin <colin.diesh@gmail.com>
2023-11-01 02:52:59 -0400

    Small cleanup

---
+++ README.md
@@ -0,0 +1,29 @@
+# maf2bed
+
+Used to convert multiple alignment format (MAF) files to a bed tabix-y style
+format. Used in jbrowse mafviewer plugin
+https://github.com/cmdcolin/jbrowse-plugin-mafviewer
+
+## Usage
+
+Make sure to specify the 'assembly name' being used as the reference for the bed
+file as the first argument to maf2bed
+
+```
+zcat file.maf.gz | maf2bed assembly_name > file.bed
+bgzip file.bed
+tabix file.bed
+```
+
+## Footnote
+
+Converted to rust from perl as a coding exercise mostly, gaining a modest
+speedup on the way
+
+## Motivation
+
+I wanted to try using the bigMaf (bigBed based) format ecosystem with large MAF
+files but bedToBigBed doesn't support streaming or reading compressed files(?),
+so that requires reading big files on disk and in memory. in contrast, MAF tabix
+type approach like implemented here can be compressed and streaming which allows
+much low memory usage and disk space
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,52 @@
 use std::{env, io};
-
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        panic!("Provide assembly name e.g. hg38 as argument")
-    }
-    let assembly_name = &args[1];
-    let mut chrom = "".to_string(); //: Option<String> = None;
-    let mut buffer = Vec::new();
+    let asm = &args[1];
+    let mut chr = "".to_string();
+    let mut b = Vec::new();
     let mut start = 0;
     let mut end = 0;
     let mut id = 0;
     let mut score = 0.0;
-    let lines = io::stdin().lines();
-    for line in lines {
+    for line in io::stdin().lines() {
         let s = line.unwrap();
         if s.starts_with("s") {
             let r: Vec<&str> = s.split_ascii_whitespace().collect();
-            let pref = format!("{}.", assembly_name);
+            let pref = format!("{}.", asm);
             if r[1].starts_with(&pref) {
-                // println!("{} yy {}", r[2], r[3]);
                 let stripped = r[1].strip_prefix(&pref);
-                chrom = stripped.unwrap().to_string();
+                chr = stripped.unwrap().to_string();
                 start = r[2].parse::<i32>().unwrap();
                 let len = r[3].parse::<i32>().unwrap();
                 end = start + len;
-                buffer.push(r[1..].join(":"))
-            } else {
-                buffer.push(r[1..].join(":"))
             }
+            b.push(r[1..].join(":"))
         } else if s.starts_with("a") {
             score = s.strip_prefix("a score=").unwrap().parse::<f32>().unwrap();
             if id > 0 {
-                print!(
+                println!(
                     "{}\t{}\t{}\t{}_{}\t{}\t{}",
-                    chrom,
+                    chr,
                     start,
                     end,
-                    assembly_name,
+                    asm,
                     id,
                     score,
-                    buffer.join(",")
+                    b.join(",")
                 );
             }
             id = id + 1;
-            buffer.clear();
+            b.clear();
         }
     }
-    print!(
+    println!(
         "{}\t{}\t{}\t{}_{}\t{}\t{}",
-        chrom,
+        chr,
         start,
         end,
-        assembly_name,
+        asm,
         id,
         score,
-        buffer.join(",")
+        b.join(",")
     );
 }
